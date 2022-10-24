54309e99d56afe0abc5c7b944676659fdc7ba655
Cripticpiramide <beau.sas@student.howest.be>
2022-10-24 13:34:21 +0200

    OOP_Refresh_EX2

---
+++ Week_6/Refresh/ijsbolletjes.py
@@ -0,0 +1,64 @@
+def exercise_seperator(exercisenr):
+    print()
+    print("OEFENING ",exercisenr)
+    print("------------")
+    print()
+
+
+#Oefening 1
+exercise_seperator(1)
+class Bolletje:
+    def __init__(self, smaak):
+        self.smaak = smaak
+
+def maak_bolletjes():
+    bolletje_1 = Bolletje("Vanille")
+    bolletje_2 = Bolletje("Chocolade")
+    bolletje_3 = Bolletje("Banaan")
+
+    bolletjes = [bolletje_1.smaak,bolletje_2.smaak,bolletje_3.smaak]
+
+    for items in bolletjes:
+        print(items)
+
+maak_bolletjes()
+
+#Oefening 2
+exercise_seperator(2)
+
+class Hoorntjes:
+    def __init__(self):
+        self.bolletjes = []
+
+    def bolletjes_toevoegen(self, *nieuwe_bolletjes):
+        for singleB in nieuwe_bolletjes:
+            self.bolletjes.append(singleB)
+    
+    def single_bol_toevoegen(self, nieuwe_bolletjes):
+        self.bolletjes.append(nieuwe_bolletjes)
+
+    def __str__(self):
+        convertedlist = []
+        for bollen in self.bolletjes:
+            convertedlist.append(bollen.smaak)
+        return "\n".join(convertedlist)
+
+ijsbak1 = Bolletje("Smurf")
+ijsbak2 = Bolletje("Pistache")
+ijsbak3 = Bolletje("Cookie Dough")
+ijsbak4 = Bolletje("Mint")
+ijsbak5 = Bolletje("Strawberry")
+
+hoorntje1 = Hoorntjes()
+hoorntje1.single_bol_toevoegen(ijsbak1)
+print()
+print("Eén smaak besteld: ",hoorntje1)
+print()
+hoorntje1.bolletjes_toevoegen(ijsbak3,ijsbak5)
+print()
+print("Meerdere smaken besteld:\n",hoorntje1)
+print()
+hoorntje1.bolletjes_toevoegen(ijsbak4)
+print()
+print("Meerdere smaken, yet we only add 1:\n",hoorntje1)
+print()
