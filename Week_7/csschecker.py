b09e6c027df084e2dbd43d5e03c01dc9bf34e0c5
Cripticpiramide <beau.sas@student.howest.be>
2022-11-07 13:09:05 +0100

    CSS-Checker_Pilot

---
+++ Week_7/csschecker.py
@@ -0,0 +1,29 @@
+import cssutils
+
+sheet = cssutils
+
+'''
+CHECKLIST:
+----------
+
+- Parse css bestand
+- Geef percentuele accuraatheid
+- Score op 20
+
+Idea?:
+------
+
+- Put elements in list, sort list from both CSS files
+
+'''
+
+
+class Reader:
+    def __init__(self):
+        pass
+    
+    def checkOneFile(self):
+        pass
+
+    def checkMoreFiles(self,amount):
+        pass
